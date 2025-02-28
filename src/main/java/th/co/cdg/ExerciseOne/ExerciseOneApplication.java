dc6beb068523e7f173ce57cce668d6935a98de56
BooPassakorn <t.passakorn4999@gmail.com>
2025-02-28 16:20:47 +0700

    Initial commit

---
+++ src/main/java/th/co/cdg/ExerciseOne/ExerciseOneApplication.java
@@ -0,0 +1,13 @@
+package th.co.cdg.ExerciseOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/co/cdg/ExerciseOne/ExerciseOneApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.cdg.ExerciseOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
